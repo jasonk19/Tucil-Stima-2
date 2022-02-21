823b1af7039fd5cc3afa0c04e83db2e3300fc77d
jasonk19 <13520080@std.stei.itb.ac.id>
2022-02-22 00:14:22 +0700

    add points sorting

--- src/myConvexHull.py
+++ src/myConvexHull.py
@@ -1,2 +1,46 @@
-def myConvexHull():
-  return "Hello World"
+# Melakukan sorting array of points terurut membesar
+# Algoritma yang digunakan untuk sorting adalah Merge Sort
+def MergeSort(points):
+  if len(points) > 1:
+    idxMid = len(points) // 2
+    leftArr = points[:idxMid]
+    rightArr = points[idxMid:]
+
+    # Pemanggilan rekursif
+    MergeSort(leftArr)
+    MergeSort(rightArr)
+
+    # iterator
+    i = 0
+    j = 0
+    k = 0
+
+    while i < len(leftArr) and j < len(rightArr):
+      if leftArr[i][0] <= rightArr[j][0]:
+        points[k] = leftArr[i]
+        i += 1
+      else:
+        points[k] = rightArr[j]
+        j += 1
+      
+      k += 1
+    
+    while i < len(leftArr):
+      points[k] = leftArr[i]
+      i += 1
+      k += 1
+    
+    while j < len(rightArr):
+      points[k] = rightArr[j]
+      j += 1
+      k += 1
+    
+
+
+
+def myConvexHull(points):
+  MergeSort(points)
+
+  return points
+
+
