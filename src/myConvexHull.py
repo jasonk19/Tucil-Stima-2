af265afba6265654c79976c467522e37bd7188f5
jasonk19 <13520080@std.stei.itb.ac.id>
2022-02-25 00:50:32 +0700

    add convex hull algorithm

--- src/myConvexHull.py
+++ src/myConvexHull.py
@@ -43,13 +43,101 @@ def getExtreme(points):
 
   return p1, pn
 
+def getLocation(p1, p2, p3):
+  value = (p1[0] * p2[1]) + (p3[0] * p1[1]) + (p2[0] * p3[1]) - (p3[0] * p2[1]) - (p2[0] * p1[1]) - (p1[0] * p3[1])
+
+  if value > 0: # Jika value positif, maka titik p3 di kiri/atas garis p1,p2
+    return 1
+  elif value < 0: # jika value negatif, maka titik p3 di kanan/bawah garis p1,p2
+    return -1
+  else:
+    return 0
+
+def getLineDistance(p1, p2, p3):
+  value = (p1[0] * p2[1]) + (p3[0] * p1[1]) + (p2[0] * p3[1]) - (p3[0] * p2[1]) - (p2[0] * p1[1]) - (p1[0] * p3[1])
+
+  return abs(value)
+
+
+def area(p1, p2, p3):
+  return abs((p1[0] * (p2[1] - p3[1]) + p2[0] * (p3[1] - p1[1]) + p3[0] * (p1[1] - p2[1]) / 2))
+
+def isInsideTriangle(p1, p2, p3, p):
+  A = area(p1, p2, p3)
+  A1 = area(p, p2, p3)
+  A2 = area(p1, p, p3)
+  A3 = area(p1, p2, p)
+
+  if (A == A1 + A2 + A3):
+    return True
+  else:
+    return False
+
+# Algoritma mencari convex hull dengan algoritma divide and conquer
+def findHull(hull, sn, p, q):
+  if len(sn) == 0:
+    return
+
+  pMax = [0, 0]
+  maxDist = 0
+
+  for point in sn:
+    if getLineDistance(p, q, point) > maxDist:
+      maxDist = getLineDistance(p, q, point)
+      pMax = point
+      
+  sn.remove(pMax)
+
+  if p in sn:
+    sn.remove(p)
+  if q in sn:
+    sn.remove(q)
+
+  hull.append(pMax)
+
+  s1 = []
+  s2 = []
+
+  for x in sn:
+    if isInsideTriangle(p, pMax, q, x):
+      sn.remove(x)
+    if (not isInsideTriangle(p, pMax, q, x)):
+      if getLocation(p, pMax, x) > 0:
+        s1.append(x)
+      if getLocation(pMax, q, x) > 0:
+        s2.append(x)
+
+  findHull(hull, s1, p, pMax)
+  findHull(hull, s2, pMax, q)
+
+
 def myConvexHull(points):
   convert_points = np.ndarray.tolist(points)
+  sorted_points = np.ndarray.tolist(points)
   
   # Lakukan pengurutan titik titik secara menaik
-  MergeSort(convert_points)
+  MergeSort(sorted_points)
 
-  # Ambil dua titik ekstrim paling kiri dan paling kanan
-  p1, pn = getExtreme(convert_points)
+  # Ambil titik ekstrim inisial
+  p1, pn = getExtreme(sorted_points)
 
-  return convert_points
+  hull = [] # Inisiasi list of points yang membentuk convex hull
+
+  # masukkan titik ekstrim ke hull
+  hull.append(p1)
+  hull.append(pn)
+
+  # bagi kumpulan titik menjadi 2 bagian
+  s1 = []
+  s2 = []
+
+  for point in sorted_points:
+    if getLocation(p1, pn, point) > 0:
+      s1.append(point) # kumpulan titik sisi atas
+    if getLocation(p1, pn, point) < 0:
+      s2.append(point) # kumpulan titik sisi bawah
+
+  findHull(hull, s1, p1, pn)
+  findHull(hull, s2, pn, p1)
+
+  return hull
